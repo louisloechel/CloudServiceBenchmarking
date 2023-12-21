5610b1d3cff4ef045298cdb608f3884b3779d21a
louisloechel <57091589+louisloechel@users.noreply.github.com>
2023-12-21 14:26:35 +0100

    fixed basic setup

--- client/main.go
+++ client/main.go
@@ -24,16 +24,20 @@ func main() {
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
 
-	// Contact the server and print out its response.
-	name := defaultName
-	if len(os.Args) > 1 {
-		name = os.Args[1]
+	for {
+		// Contact the server and print out its response.
+		name := defaultName
+		if len(os.Args) > 1 {
+			name = os.Args[1]
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		cancel()
+		log.Printf("Greeting: %s", r.GetMessage())
+
+		time.Sleep(5 * time.Second) // Sleep for 5 seconds
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.GetMessage())
 }
