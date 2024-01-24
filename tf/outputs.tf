cb9047900a23525c3e1ea7c1071a2d23f3a69500
louisloechel <57091589+louisloechel@users.noreply.github.com>
2024-01-24 11:29:59 +0100

    add server_internap_ip output

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,3 +1,8 @@
 output "vm_public_ip" {
   value = google_compute_instance.client_instance.network_interface[0].access_config[0].nat_ip
 }
+
+output "server_internal_ip" {
+  value = google_compute_instance.server_instance.network_interface[0].network_ip
+}
+
