20adf7d0091b56e86c99b3bec15cf9b3e76e7c6a
louisloechel <57091589+louisloechel@users.noreply.github.com>
2024-05-14 15:24:10 +0200

    reduced experiment setup

--- client/main.go
+++ client/main.go
@@ -218,7 +218,7 @@ func runBenchmark(c pb.GreeterClient, concurrentRequests int, config Config) {
 
 			// Deactivate random token selection
 			// Options: generalizedToken, noisedToken, reducedToken, mixedToken, goodToken, badToken (see above)
-			token = noisedToken
+			token = reducedToken
 
 			// Custom auth
 			md := grpcMetadata.Pairs("authorization", token)
